1b5d32d03ffa725f1819ee4112c7d938febb5778
mustafaarif85 <32451438+mustafaarif85@users.noreply.github.com>
2017-10-16 12:29:19 +0500

    Initial commit

---
+++ FibonacciSeries/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> series = new List<int>();
+            int number = 1 , item=0 ;
+            int limit = 4000000;
+            int evenSum = 0;        
+
+
+            while (number <= limit)
+            {
+                number = (item > 1 ? 0 : number) + (item == 0 ? 0 : series[item - 1]);
+                if (item > 1) { number = number + series[item - 2]; }
+                if (number < limit)
+                {
+                    series.Add(number);
+                    Console.Write(number.ToString() + ",  ");
+                    if (number % 2 == 0)
+                    { evenSum = evenSum + number; }                   
+                    item++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(string.Format("Sum of even terms in the Fibonacci Series is : {0}",evenSum));
+            Console.ReadKey();
+        }       
+    }
+}
